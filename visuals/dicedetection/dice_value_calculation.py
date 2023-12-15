e5fc62f194e5ef276db27548ee37adb29dfed2fa
Gergely Bodi <gergely_bodi@epam.com>
2023-12-15 18:48:52 +0100

    removed unused method

--- visuals/dicedetection/dice_value_calculation.py
+++ visuals/dicedetection/dice_value_calculation.py
@@ -20,17 +20,3 @@ def calculate_dice_roll_from_move(removed_from, moved_to):
                 if (i == distances[0] and j == distances[1]) or (i == distances[1] and j == distances[0]):
                     possible_rolls.add((min(i, j), max(i, j)))
         return list(possible_rolls)
-
-
-def get_most_likely_dice_roll(possible_dice_rolls, removed_from, moved_to):
-    # Calculate the absolute differences between the starting and ending positions
-    distances = [abs(end - start) for start, end in zip(removed_from, moved_to)]
-
-    # Count how many times each possible dice roll appears in the distances
-    dice_counts = {dice: distances.count(dice) for dice in possible_dice_rolls}
-
-    # Sort the possible dice rolls based on their counts in descending order
-    sorted_dice = sorted(dice_counts.keys(), key=lambda x: dice_counts[x], reverse=True)
-
-    # Return the two most likely dice rolls
-    return sorted_dice[:2]
